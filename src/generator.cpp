0fa035d8d13eafa471c786d214e4b9a5122d0cad
Coding Jerk <codingjerk@gmail.com>
2014-07-08 22:02:20 +0400

    Generator: rooks was finaly writed

--- src/generator.cpp
+++ src/generator.cpp
@@ -15,51 +15,29 @@
 using namespace Generator;
 
 // --- Rooks
-int m_rOffset[64];
-Bitboard::Type *m_rData;
+UNumspeed rookOffsets[64];
+Bitboard::Type *rookData = nullptr;
 
-const Bitboard::Type m_rMask[64] =
-{
-    0x7e80808080808000, 0x3e40404040404000, 0x5e20202020202000, 0x6e10101010101000,
-    0x7608080808080800, 0x7a04040404040400, 0x7c02020202020200, 0x7e01010101010100,
-    0x007e808080808000, 0x003e404040404000, 0x005e202020202000, 0x006e101010101000,
-    0x0076080808080800, 0x007a040404040400, 0x007c020202020200, 0x007e010101010100,
-    0x00807e8080808000, 0x00403e4040404000, 0x00205e2020202000, 0x00106e1010101000,
-    0x0008760808080800, 0x00047a0404040400, 0x00027c0202020200, 0x00017e0101010100,
-    0x0080807e80808000, 0x0040403e40404000, 0x0020205e20202000, 0x0010106e10101000,
-    0x0008087608080800, 0x0004047a04040400, 0x0002027c02020200, 0x0001017e01010100,
-    0x008080807e808000, 0x004040403e404000, 0x002020205e202000, 0x001010106e101000,
-    0x0008080876080800, 0x000404047a040400, 0x000202027c020200, 0x000101017e010100,
-    0x00808080807e8000, 0x00404040403e4000, 0x00202020205e2000, 0x00101010106e1000,
-    0x0008080808760800, 0x00040404047a0400, 0x00020202027c0200, 0x00010101017e0100,
-    0x0080808080807e00, 0x0040404040403e00, 0x0020202020205e00, 0x0010101010106e00,
-    0x0008080808087600, 0x0004040404047a00, 0x0002020202027c00, 0x0001010101017e00,
-    0x008080808080807e, 0x004040404040403e, 0x002020202020205e, 0x001010101010106e,
-    0x0008080808080876, 0x000404040404047a, 0x000202020202027c, 0x000101010101017e
+const UNum64 rookMagics[64] = {
+    0x00800011400080a6, 0x004000100120004e, 0x0080100008600082, 0x0080080016500080, 
+    0x0080040008000280, 0x0080020005040080, 0x0080108046000100, 0x0080010000204080, 
+    0x0010800424400082, 0x00004002c8201000, 0x000c802000100080, 0x00810010002100b8, 
+    0x00ca808014000800, 0x0002002884900200, 0x0042002148041200, 0x00010000c200a100, 
+    0x00008580004002a0, 0x0020004001403008, 0x0000820020411600, 0x0002120021401a00, 
+    0x0024808044010800, 0x0022008100040080, 0x00004400094a8810, 0x0000020002814c21, 
+    0x0011400280082080, 0x004a050e002080c0, 0x00101103002002c0, 0x0025020900201000, 
+    0x0001001100042800, 0x0002008080022400, 0x000830440021081a, 0x0080004200010084, 
+    0x00008000c9002104, 0x0090400081002900, 0x0080220082004010, 0x0001100101000820, 
+    0x0000080011001500, 0x0010020080800400, 0x0034010224009048, 0x0002208412000841, 
+    0x000040008020800c, 0x001000c460094000, 0x0020006101330040, 0x0000a30010010028, 
+    0x0004080004008080, 0x0024000201004040, 0x0000300802440041, 0x00120400c08a0011, 
+    0x0080006085004100, 0x0028600040100040, 0x00a0082110018080, 0x0010184200221200, 
+    0x0040080005001100, 0x0004200440104801, 0x0080800900220080, 0x000a01140081c200, 
+    0x0080044180110021, 0x0008804001001225, 0x00a00c4020010011, 0x00001000a0050009, 
+    0x0011001800021025, 0x00c9000400620811, 0x0032009001080224, 0x001400810044086a
 };
 
-const UNum64 m_rMult[64] =
-{
-    0x0000010824004082, 0x0001004200008c01, 0x0401008400020801, 0x0082001020040802,
-    0x0000040861001001, 0x0000084100600011, 0x0001001040820022, 0x00008000c1001021,
-    0x0010408400410200, 0x0100050810020400, 0x8400800200840080, 0x0200940080080080,
-    0x0080082090010100, 0x0018802000100880, 0x0010114008200040, 0x0008248000c00080,
-    0x2800008044020001, 0x2000020810040001, 0x0011000804010042, 0x0000842801010010,
-    0x2010008100080800, 0x0010200100430010, 0x3008201000404000, 0x0005400060808000,
-    0x4000088042000401, 0x0000020104001810, 0x0802008002804400, 0x0101140080800800,
-    0x0220801000800800, 0x0800881000802001, 0x0808601000404000, 0x4000804000801120,
-    0x0020040200004481, 0x0041000100820004, 0x8200040080800600, 0x0201180080040080,
-    0x1000402200120008, 0x0a00100080802000, 0x0120002180400080, 0x4020248080004000,
-    0x0080020005008044, 0x0820010100020004, 0x0040818004002200, 0x0400808008000401,
-    0x0808008010008008, 0x0000410020001504, 0x2008808040002001, 0x2008808000224000,
-    0x0000800048800100, 0x0020800100802200, 0x2000802200808400, 0x0002000600201810,
-    0x0908808010000800, 0x0101801004200080, 0x00204010022000c0, 0x0080800120804004,
-    0x0080004031000880, 0x1280008001004200, 0x0200020001440810, 0x1001000840208010,
-    0x010020100009000c, 0x0880100008200080, 0x0040084020005002, 0x0080002882104000
-};
-
-const int m_rBits[64] =
-{
+const UNumspeed rookMaskBits[64] = {
     12, 11, 11, 11, 11, 11, 11, 12,
     11, 10, 10, 10, 10, 10, 10, 11,
     11, 10, 10, 10, 10, 10, 10, 11,
@@ -70,12 +48,24 @@ const int m_rBits[64] =
     12, 11, 11, 11, 11, 11, 11, 12
 };
 
+const UNumspeed rookMaskShifts[64] = {
+    64 - 12, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 12,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 11, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 10, 64 - 11,
+    64 - 12, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 11, 64 - 12
+};
+
 // --- Rooks
 
 // @TODO: Move to tables.cpp, hpp
 Bitboard::Type knightMasks[makeUNumspeed(1) << Coord::usedBits];
 Bitboard::Type kingMasks[makeUNumspeed(1) << Coord::usedBits];
 Bitboard::Type bishopMasks[makeUNumspeed(1) << Coord::usedBits];
+Bitboard::Type rookMasks[makeUNumspeed(1) << Coord::usedBits];
 
 // @TODO: Move to bitboard.cpp, hpp
 Bitboard::Type pawnStartLine[makeUNumspeed(1) << Color::usedBitsReal];
@@ -89,7 +79,7 @@ Bitboard::Type downLine;
 // Magics @TODO: Move to own file
 UNumspeed bishopOffsets[64];
 
-Bitboard::Type *bishopData;
+Bitboard::Type *bishopData = nullptr;
 
 const UNumspeed bishopMaskBits[64] = {
     6,  5,  5,  5,  5,  5,  5,  6,
@@ -214,12 +204,10 @@ template<> void forKing<Black>(MoveBuffer &buffer, const Board::Type &board, con
     }
 }
 
-#include <iostream>
-
 template <Color::Type COLOR>
 void forBishop(MoveBuffer &buffer, const Board::Type &board, const Coord::Type from) {
     Bitboard::Type nonEmpty = (board.bitboards[Black] | board.bitboards[White]);
-    const UNumspeed magicIndex = bishopOffsets[from] + int(((nonEmpty & bishopMasks[from]) * bishopMagics[from]) >> (bishopMaskShifts[from]));
+    const UNumspeed magicIndex = bishopOffsets[from] + UNumspeed(((nonEmpty & bishopMasks[from]) * bishopMagics[from]) >> (bishopMaskShifts[from]));
 
     addLegals(buffer, from, bishopData[magicIndex] & (~board.bitboards[COLOR]));
 }
@@ -228,11 +216,9 @@ void forBishop(MoveBuffer &buffer, const Board::Type &board, const Coord::Type f
 template <Color::Type COLOR>
 void forRook(MoveBuffer &buffer, const Board::Type &board, const Coord::Type from) {
     Bitboard::Type nonEmpty = (board.bitboards[Black] | board.bitboards[White]);
-    const UNumspeed magicIndex = m_rOffset[63-from] + int(((nonEmpty & m_rMask[63-from]) * m_rMult[63-from]) >> (64 - m_rBits[from]));
+    const UNumspeed magicIndex = rookOffsets[from] + UNumspeed(((nonEmpty & rookMasks[from]) * rookMagics[from]) >> (rookMaskShifts[from]));
 
-    std::cout << Bitboard::show(m_rData[magicIndex] & (~board.bitboards[COLOR])) << "\n";
-
-    addLegals(buffer, from, m_rData[magicIndex] & (~board.bitboards[COLOR]));
+    addLegals(buffer, from, rookData[magicIndex] & (~board.bitboards[COLOR]));
 }
 
 void Generator::forKnights(MoveBuffer &buffer, const Board::Type &board) {
@@ -460,19 +446,17 @@ template void Generator::forKnights<Black>(MoveBuffer&, const Board::Type&);
 template void Generator::forKings<White>(MoveBuffer&, const Board::Type&);
 template void Generator::forKings<Black>(MoveBuffer&, const Board::Type&);
 
-#include <iostream>
-
 // @TODO: Move to bitboards.cpp, hpp
 Bitboard::Type turnBits(Bitboard::Type mask, UNumspeed bitsToTurn) {
     Bitboard::Type result = 0;
 
     while (mask != 0 && bitsToTurn != 0) {
-        UNumspeed f = Bitboard::bitScan(mask);
-        mask ^= Bitboard::fromIndex(f);
+        UNumspeed first = Bitboard::bitScan(mask);
+        mask ^= Bitboard::fromIndex(first);
 
         UNumspeed digit = UNumspeed(bitsToTurn & 1);
         bitsToTurn >>= 1;
-        result |= digit * Bitboard::fromIndex(f);
+        result |= digit * Bitboard::fromIndex(first);
     }
 
     return result;
@@ -489,64 +473,84 @@ Bitboard::Type upLeft(const Bitboard::Type b) { return (b & ~(leftLine|upLine)) 
 Bitboard::Type downRight(const Bitboard::Type b) { return (b & ~(rightLine|downLine)) >> 7; }
 Bitboard::Type downLeft(const Bitboard::Type b) { return (b & ~(leftLine|downLine)) >> 9; }
 
-Bitboard::Type calculateBishopAttacks(int f, const Bitboard::Type& occ)
+Bitboard::Type calculateBishopAttacks(UNumspeed from, const Bitboard::Type nonEmpty)
 {
     Bitboard::Type result = 0;
     Bitboard::Type x = 0;
 
-    x = upRight(Bitboard::fromIndex(f)); 
+    x = upRight(Bitboard::fromIndex(from)); 
     while (x)                       
     {                               
         result |= x;                   
-        if (x & occ) break;         
+        if (x & nonEmpty) break;         
         x = upRight(x);               
     }
 
-    x = upLeft(Bitboard::fromIndex(f)); 
+    x = upLeft(Bitboard::fromIndex(from)); 
     while (x)                       
     {                               
         result |= x;                   
-        if (x & occ) break;         
+        if (x & nonEmpty) break;         
         x = upLeft(x);               
     }
 
-    x = downRight(Bitboard::fromIndex(f)); 
+    x = downRight(Bitboard::fromIndex(from)); 
     while (x)                       
     {                               
         result |= x;                   
-        if (x & occ) break;         
+        if (x & nonEmpty) break;         
         x = downRight(x);               
     }
 
-    x = downLeft(Bitboard::fromIndex(f)); 
+    x = downLeft(Bitboard::fromIndex(from)); 
     while (x)                       
     {                               
         result |= x;                   
-        if (x & occ) break;         
+        if (x & nonEmpty) break;         
         x = downLeft(x);               
     }
 
     return result;
 }
 
-#define TRACE(Shift)                \
-    x = Shift(Bitboard::fromIndex(f)); \
-    while (x)                       \
-    {                               \
-        att |= x;                   \
-        if (x & occ) break;         \
-        x = Shift(x);               \
+Bitboard::Type calculateRookAttacks(UNumspeed from, const Bitboard::Type nonEmpty)
+{
+    Bitboard::Type result = 0;
+    Bitboard::Type x = 0;
+
+    x = up(Bitboard::fromIndex(from)); 
+    while (x)                       
+    {                               
+        result |= x;                   
+        if (x & nonEmpty) break;         
+        x = up(x);               
     }
 
-Bitboard::Type RookAttacksTrace(int f, const Bitboard::Type& occ)
-{
-    Bitboard::Type att = 0;
-    Bitboard::Type x = 0;
-    TRACE(right);
-    TRACE(up);
-    TRACE(left);
-    TRACE(down);
-    return att;
+    x = down(Bitboard::fromIndex(from)); 
+    while (x)                       
+    {                               
+        result |= x;                   
+        if (x & nonEmpty) break;         
+        x = down(x);               
+    }
+
+    x = right(Bitboard::fromIndex(from)); 
+    while (x)                       
+    {                               
+        result |= x;                   
+        if (x & nonEmpty) break;         
+        x = right(x);               
+    }
+
+    x = left(Bitboard::fromIndex(from)); 
+    while (x)                       
+    {                               
+        result |= x;                   
+        if (x & nonEmpty) break;         
+        x = left(x);               
+    }
+
+    return result;
 }
 
 void Generator::initTables() {
@@ -601,6 +605,28 @@ void Generator::initTables() {
         }
 
         bishopMasks[from] = toBits;
+    }
+
+    forCoord(x)
+    forCoord(y) {
+        const auto from = Coord::create(x, y);
+        auto toBits = Bitboard::null;
+
+        for (Numspeed xTo = 1; xTo <= 6; ++xTo) {
+            const auto to = Coord::create(xTo, y);
+
+            toBits |= Bitboard::fromCoord(to);
+        }
+
+        for (Numspeed yTo = 1; yTo <= 6; ++yTo) {
+            const auto to = Coord::create(x, yTo);
+
+            toBits |= Bitboard::fromCoord(to);
+        }
+
+        toBits &= ~Bitboard::fromCoord(from);
+
+        rookMasks[from] = toBits;
     }
 
     //@TODO: Hardcode this definitions
@@ -705,22 +731,22 @@ void Generator::initTables() {
     forCoord(x)
     forCoord(y) {
         auto const from = Coord::create(x, y);
-        m_rOffset[from] = offset;
-        offset += (1 << m_rBits[from]);
+        rookOffsets[from] = offset;
+        offset += (1 << rookMaskBits[from]);
     }
-    m_rData = new Bitboard::Type[offset];
+    rookData = new Bitboard::Type[offset];
 
-    for (int f = 0; f < 64; ++f)
-    {
-        Bitboard::Type mask = m_rMask[63-f];
-        int bits = m_rBits[63-f];
-        for (int n = 0; n < (1 << bits); ++n)
-        {
-            Bitboard::Type occ = turnBits(mask, n);
-            Bitboard::Type att = RookAttacksTrace(f, occ);
-            int index = m_rOffset[63-f];
-            index += int((occ * m_rMult[63-f]) >> (64 - bits));
-            m_rData[index] = att;
+    forCoord(x)
+    forCoord(y) {
+        auto const from = Coord::create(x, y);
+
+        Bitboard::Type mask = rookMasks[from];
+        UNumspeed bits = rookMaskBits[from];
+        for (UNumspeed bitsToTurn = makeUNumspeed(0); bitsToTurn < (makeUNumspeed(1) << bits); ++bitsToTurn) {
+            Bitboard::Type nonEmpty = turnBits(mask, bitsToTurn);
+            Bitboard::Type data = calculateRookAttacks(from, nonEmpty);
+            UNumspeed index = rookOffsets[from] + UNumspeed((nonEmpty * rookMagics[from]) >> (makeUNumspeed(64) - bits));
+            rookData[index] = data;
         }
     }
 }
