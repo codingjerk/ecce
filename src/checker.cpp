360659d5e6849c288c35134796b93aad018fe603
Coding Jerk <codingjerk@gmail.com>
2014-07-09 23:08:19 +0400

    Checker::isCheck

---
+++ src/checker.cpp
@@ -0,0 +1,56 @@
+#include "checker.hpp"
+
+#include "bitboards.hpp"
+#include "generatorTables.hpp"
+#include "magics.hpp"
+
+using namespace Checker;
+
+template <Color::Type WHO> 
+Boolspeed Checker::isCheck(const Board::Type &board) {
+    //@TODO: Rewrite in king-centric style
+    Bitboard::Type attacks = Bitboard::null;
+
+    const auto OPP = Color::inv(WHO);
+
+    auto bitboard = board.bitboards[Piece::create(OPP, Knight)];
+    while(bitboard != Bitboard::null) {
+        const auto from = Bitboard::bitScan(bitboard);
+        bitboard ^= Bitboard::fromIndex(from);
+
+        attacks |= (~board.bitboards[OPP]) & Tables::knightMasks[from];
+    }
+
+    Bitboard::Type nonEmpty = (board.bitboards[Black] | board.bitboards[White]);
+    bitboard = board.bitboards[Piece::create(OPP, Bishop)] | board.bitboards[Piece::create(OPP, Queen)];
+    while(bitboard != Bitboard::null) {
+        const auto from = Bitboard::bitScan(bitboard);
+        bitboard ^= Bitboard::fromIndex(from);
+
+        const UNumspeed magicIndex = Magic::bishopOffsets[from] 
+            + UNumspeed(((nonEmpty & Tables::bishopMasks[from]) * Magic::bishopMagics[from]) 
+                >> (Magic::bishopMaskShifts[from]));
+
+        attacks |= Magic::bishopData[magicIndex] & (~board.bitboards[OPP]);
+    }
+
+    bitboard = board.bitboards[Piece::create(OPP, Rook)] | board.bitboards[Piece::create(OPP, Queen)];
+    while(bitboard != Bitboard::null) {
+        const auto from = Bitboard::bitScan(bitboard);
+        bitboard ^= Bitboard::fromIndex(from);
+
+        const UNumspeed magicIndex = Magic::rookOffsets[from] 
+            + UNumspeed(((nonEmpty & Tables::rookMasks[from]) * Magic::rookMagics[from]) 
+                >> (Magic::rookMaskShifts[from]));
+
+        attacks |= Magic::rookData[magicIndex] & (~board.bitboards[OPP]);
+    }
+
+    //@TODO: Pawns checks
+
+    return (board.bitboards[Piece::create(WHO, King)] & attacks);
+}
+
+// Explicit template instantiations
+template Boolspeed Checker::isCheck<White>(const Board::Type&);
+template Boolspeed Checker::isCheck<Black>(const Board::Type&);
---
+++ src/checker.hpp
@@ -0,0 +1,12 @@
+#ifndef CHECKER_HPP
+#define CHECKER_HPP
+
+#include "core.hpp"
+
+#include "boards.hpp"
+
+namespace Checker {
+    template <Color::Type WHO> Boolspeed isCheck(const Board::Type&);
+}
+
+#endif /* CHECKER_HPP */
