5f4890c20ccfc9352d73713dcb4168629c8e3770
Coding Jerk <codingjerk@gmail.com>
2014-07-14 22:58:37 +0400

    Added OSLINUX define, for uncross-platform features

--- src/core.hpp
+++ src/core.hpp
@@ -1,6 +1,10 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
+// --- OS ---
+// Remove this for compile on windows with visual studio
+#define OSLINUX
+
 // --- Arch-depended types ---
 using Boolspeed = unsigned long long;
 
