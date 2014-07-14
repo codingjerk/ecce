d9b4bbb0fcdb0a5cdbf62f881efe874a870c37d4
Coding Jerk <codingjerk@gmail.com>
2014-07-15 03:18:20 +0400

    All pieces added to evaluation.

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -7,9 +7,17 @@ template <>
 Score::Type material<White>(Board::Type& board) {
     Score::Type result = 0;
 
-    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Pawn)]) * Score::Pawn;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Pawn)])   * Score::Pawn;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Knight)]) * Score::Knight;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Bishop)]) * Score::Bishop;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Rook)])   * Score::Rook;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(White, Queen)])  * Score::Queen;
     
-    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Pawn)]) * Score::Pawn;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Pawn)])   * Score::Pawn;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Knight)]) * Score::Knight;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Bishop)]) * Score::Bishop;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Rook)])   * Score::Rook;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(Black, Queen)])  * Score::Queen;
 
     return result;
 }
@@ -18,9 +26,17 @@ template <>
 Score::Type material<Black>(Board::Type& board) {
     Score::Type result = 0;
     
-    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Pawn)]) * Score::Pawn;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Pawn)])   * Score::Pawn;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Knight)]) * Score::Knight;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Bishop)]) * Score::Bishop;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Rook)])   * Score::Rook;
+    result += Bitboard::enabledCount(board.bitboards[Piece::create(Black, Queen)])  * Score::Queen;
 
-    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Pawn)]) * Score::Pawn;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Pawn)])   * Score::Pawn;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Knight)]) * Score::Knight;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Bishop)]) * Score::Bishop;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Rook)])   * Score::Rook;
+    result -= Bitboard::enabledCount(board.bitboards[Piece::create(White, Queen)])  * Score::Queen;
 
     return result;
 }
