0528a5bc90a53d5a9ada2a11f5f04dac3f8e53d7
Coding Jerk <codingjerk@gmail.com>
2014-10-01 16:51:05 -0700

    Some very-global todos

--- src/ecce.cpp
+++ src/ecce.cpp
@@ -1,3 +1,31 @@
+// @TODO, GLOBAL:
+//    - Best UCI
+//      - Support of go *
+//      - Support of options
+//      - Support of ponder
+//      - Support of multipv
+
+//    - Best Search
+//      - Write Simple Search (alpha-beta)
+//      - Extract PV
+//      - Write stop command
+//      - Test search
+
+//    - Best Eval
+//      - Test Eval
+
+//    - Best Time Manager
+//      - Write Simple TM
+//      - Allow to use go *
+//      - Test TM
+
+//    - Best Speed
+//      - Test current speed
+//      - Optimize low
+//      - Optimize razoring
+//      - Optimize evaluation
+//      - Optimize parallel
+
 #include "uci.hpp"
 #include "mover.hpp"
 #include "bitboards.hpp"
