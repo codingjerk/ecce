b7e362debb86e2cdc580dc87e4e8237fba08c7c2
Coding Jerk <codingjerk@gmail.com>
2014-10-06 13:16:00 -0700

    Score::show, Score::isMate and other

--- src/score.hpp
+++ src/score.hpp
@@ -1,6 +1,9 @@
 #ifndef SCORE_HPP
 #define SCORE_HPP
 
+#include <string>
+#include <sstream>
+
 #include "core.hpp"
 
 namespace Score {
@@ -16,6 +19,32 @@ namespace Score {
     const Type Queen    = 90000;
 
     const Type Infinity = 100 * Queen;
+	
+	inline bool isPositiveMate(Type score) {
+		return score >= (Infinity - Type(MAX_DEPTH));
+	}
+	
+	inline bool isNegativeMate(Type score) {
+		return score <= (-Infinity + Type(MAX_DEPTH));
+	}
+
+	inline bool isMate(Type score) {
+		return isPositiveMate(score) || isNegativeMate(score);
+	}
+
+	inline std::string show(Type score) {
+		std::stringstream ss;
+		
+		if (isPositiveMate(score)) {
+			ss << "mate " << (score - Infinity + Type(MAX_DEPTH)) / 2;
+		} else if (isNegativeMate(score)) {
+			ss << "mate " << (score + Infinity - Type(MAX_DEPTH)) / 2;
+		} else {
+			ss << "cp " << score * 100 / Pawn;
+		}
+
+		return ss.str();
+	}
 }
 
 #endif /* SCORE_HPP */
