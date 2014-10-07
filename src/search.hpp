08ae91834041f2ffa2e0cd245ae8f1fd02911c2a
Coding Jerk <codingjerk@gmail.com>
2014-10-07 13:58:50 -0700

    Speedfix - interupter checks every 2^14 times. (+20% perf)

--- src/search.hpp
+++ src/search.hpp
@@ -32,7 +32,7 @@ namespace Search {
 
         if (depth <= 0) return Eval::total<COLOR>(board);
 
-		if (totalNodes % 100 == 0) {
+		if (!(totalNodes % 16384)) {
 			if (stopSearch || isInterupt()) {
 				stopSearch = true;
 				return 0;
