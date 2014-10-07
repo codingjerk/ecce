c5f6ccb132e10af72b765fb30c029a10e0f34407
Coding Jerk <codingjerk@gmail.com>
2014-10-07 11:17:28 -0700

    TimeLimit in timemanager

--- src/tm.hpp
+++ src/tm.hpp
@@ -8,10 +8,34 @@ namespace TM {
         Numspeed maxDepth = MAX_DEPTH;
     };
 
+    struct TimeLimit {
+        Numspeed maxTime;
+    };
+
     DepthLimit depth(Numspeed aDepth) {
         DepthLimit result;
 
         result.maxDepth = aDepth;
+
+        return result;
+    }
+
+    TimeLimit time(Numspeed aTime) {
+        TimeLimit result;
+
+        result.maxTime = aTime;
+
+        return result;
+    }
+
+	TimeLimit time(Numspeed time, Numspeed inc, Numspeed moves) {
+        TimeLimit result;
+
+		if (moves != 0)
+			result.maxTime = (time + inc * moves) / moves;
+		else {
+			result.maxTime = (time + inc * 40) / 40;
+		}
 
         return result;
     }
