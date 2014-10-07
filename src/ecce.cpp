44a9055682dc634f2234bc059f8bc53d689ebaea
Coding Jerk <codingjerk@gmail.com>
2014-10-07 12:53:14 -0700

    Some global todos added/removed

--- src/ecce.cpp
+++ src/ecce.cpp
@@ -1,40 +1,26 @@
 // @TODO, GLOBAL:
 //    - Best UCI
-//      - Support of info
-//      - Support of go *
+//      - Support of info (seldepth (extract from board), matescore
 //      - Support of options
-//      - Support of ponder
-//      - Support of multipv
 
 //    - Best Search
-//      (+) - Write Simple Search (alpha-beta)
-//      (+) - Extract PV
-//      (+) - Write stop command
-//      (+) - Incremental 
 //      - Test search
 //      - Negascout
 //      - Sort LVA/MVV
-//      - Move buffer in board
 //      - Write quies
 //      - Triple replies
 //      - Hash table
 
 //    - Best Eval
-//      (+) - Material
-//      (+) - PST
 //      - Some other positional factors
 //      - Test Eval
 
 //    - Best Time Manager
-//      (+) - Write Simple TM
-//      (+) - Use depth in TM
-//      - Use time in TM
-//      - Allow to use go *
 //      - Test TM
 
 //    - Best Speed
 //      - Test current speed
-//      - Optimize low
+//      - Optimize algo
 //      - Optimize razoring
 //      - Optimize evaluation
 //      - Optimize parallel
