59a8f0911a48a67ed87c5a884ec3865e8ec81116
Coding Jerk <codingjerk@gmail.com>
2014-10-09 14:25:53 -0700

    Ref: Magics

--- src/magics.hpp
+++ src/magics.hpp
@@ -8,10 +8,10 @@ namespace Magic {
     void initTables();
 
     extern Bitboard::Type *bishopData;
-    extern UNumspeed bishopOffsets[64];
+    extern UNumspeed bishopOffsets[makeUNumspeed(1) << Coord::usedBits];
     extern const UNum64 bishopMagics[makeUNumspeed(1) << Coord::usedBits];
-    extern const UNumspeed bishopMaskBits[64];
-    extern const UNumspeed bishopMaskShifts[64];
+    extern const UNumspeed bishopMaskBits[makeUNumspeed(1) << Coord::usedBits];
+    extern const UNumspeed bishopMaskShifts[makeUNumspeed(1) << Coord::usedBits];
     
     extern Bitboard::Type *rookData;
     extern UNumspeed rookOffsets[makeUNumspeed(1) << Coord::usedBits];
