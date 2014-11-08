8af2ea893f52043ed82729683621d507e8c500cb
Coding Jerk <codingjerk@gmail.com>
2014-11-08 16:24:33 +0000

    Ref: Core

--- src/core.hpp
+++ src/core.hpp
@@ -1,42 +1,13 @@
 #ifndef CORE_HPP
 #define CORE_HPP
 
-// @TODO: Remove types from core and move it to types.hpp
+#include "types.hpp"
+
 #define OSLINUX
 #undef OSLINUX
 
 #define DEBUGMODE
 #undef DEBUGMODE
-
-// --- Arch-depended types ---
-using Boolspeed = unsigned long long;
-
-using Num64 = signed long long;
-using UNum64 = unsigned long long;
-
-using Numspeed = signed long long;
-using UNumspeed = unsigned long long;
-
-using Numsize = signed char;
-using UNumsize = unsigned char;
-
-using Nummax = signed long long;
-using UNummax = unsigned long long;
-
-// --- Additional macroses to making values
-#define makeBoolspeed(num) num##ull
-
-#define makeNum64(num) num##ll
-#define makeUNum64(num) num##ull
-
-#define makeNumspeed(num) num##ll
-#define makeUNumspeed(num) num##ull
-
-#define makeNumsize(num) num
-#define makeUNumsize(num) num
-
-#define makeNummax(num) num##ll
-#define makeUNummax(num) num##ull
 
 const UNumspeed MAX_DEPTH = makeUNumspeed(300);
 const UNumspeed MAX_MOVES = makeUNumspeed(300);
---
+++ src/types.hpp
@@ -0,0 +1,27 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+using Boolspeed = unsigned long long;
+#define makeBoolspeed(num) num##ull
+
+using Num64 = signed long long;
+#define makeNum64(num) num##ll
+using UNum64 = unsigned long long;
+#define makeUNum64(num) num##ull
+
+using Numspeed = signed long long;
+#define makeNumspeed(num) num##ll
+using UNumspeed = unsigned long long;
+#define makeUNumspeed(num) num##ull
+
+using Numsize = signed char;
+#define makeNumsize(num) num
+using UNumsize = unsigned char;
+#define makeUNumsize(num) num
+
+using Nummax = signed long long;
+#define makeNummax(num) num##ll
+using UNummax = unsigned long long;
+#define makeUNummax(num) num##ull
+
+#endif /* TYPES_HPP */
