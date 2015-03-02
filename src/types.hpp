59ba841aa5c789b5d4e79d1e7a49e647aa473000
Coding Jerk <codingjerk@gmail.com>
2015-03-02 05:42:31 +0300

    Types

--- src/types.hpp
+++ src/types.hpp
@@ -1,17 +1,17 @@
 #ifndef TYPES_HPP
 #define TYPES_HPP
 
-using Boolspeed = unsigned long long;
+using Boolspeed = long long unsigned int;
 #define makeBoolspeed(num) num##ull
 
-using Num64 = signed long long;
+using Num64 = long long signed int;
 #define makeNum64(num) num##ll
-using UNum64 = unsigned long long;
+using UNum64 = long long unsigned int;
 #define makeUNum64(num) num##ull
 
-using Numspeed = signed long long;
+using Numspeed = long long signed int;
 #define makeNumspeed(num) num##ll
-using UNumspeed = unsigned long long;
+using UNumspeed = long long unsigned int;
 #define makeUNumspeed(num) num##ull
 
 using Numsize = signed char;
@@ -19,9 +19,9 @@ using Numsize = signed char;
 using UNumsize = unsigned char;
 #define makeUNumsize(num) num
 
-using Nummax = signed long long;
+using Nummax = long long signed int;
 #define makeNummax(num) num##ll
-using UNummax = unsigned long long;
+using UNummax = long long unsigned int;
 #define makeUNummax(num) num##ull
 
 #endif /* TYPES_HPP */
